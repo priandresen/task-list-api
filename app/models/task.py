a996fedeb68ab12239f15399d871e36733ea087e
Priscilla Andresen <pgdamorim@gmail.com>
2025-10-30 16:01:03 -0700

    registers blueprint

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str] 
+    completed_at: Mapped[datetime | None] = mapped_column(nullable=True, default=None)
+    #is completed at correct? I want it to be default none unles specified with datatime
+    # If completed_at is not provided, it should default to None
+    
