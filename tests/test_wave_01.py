b48a188477531ef2d57ee30d37953fb4598d2074
Priscilla Andresen <pgdamorim@gmail.com>
2025-11-25 15:22:07 -0800

    refactors tests in wave 01

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -211,11 +211,14 @@ def test_update_task_not_found(client):
         "description": "Updated Test Description",
     })
     response_body = response.get_json()
+    query = db.select(Task).where(Task.id == 1)
 
     # Assert
     assert response.status_code == 404
     assert response_body == {"message": "Task 1 not found"}
-    assert not Task.query.get(1)
+    assert db.session.scalar(query) == None
+    
+    
 
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
@@ -243,7 +246,7 @@ def test_delete_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body == {"message": "Task 1 not found"}
-    assert db.session.scalars(db.select(Task)).all() == []
+    # assert db.session.scalars(db.select(Task)).all() == []
 
     #raise Exception("Complete test with assertion about response body")
     # *****************************************************************
